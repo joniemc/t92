2e58af09db4df8512031571ccdc0232531a76993
jonie_mirc <jonie.mirc@gmail.com>
2025-02-07 19:03:10 -0600

    Mi primer proyecto con express.js

--- holamundoexpress/index.js
+++ holamundoexpress/index.js
@@ -1,1 +1,15 @@
-console.log("Hola Mundo");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req,res)=>{
+    res.send('¡Hola Mundo de Express...!');
+});
+
+app.get('/nuevomensaje',(req,res)=>{
+    res.send('¡Este es un mensaje diferente...!');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
