cf22605c1b8aed730d6fc5ec98b5766265fb8334
jonie_mirc <jonie.mirc@gmail.com>
2025-02-12 19:16:14 -0600

    Ejercicio clase

---
+++ crud-usuarios/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/usuarios/:id/:edad',(req,res)=>{
+    const id = req.params.id;
+    const edad = req.params.edad;
+    res.status(200).json({id: id, edad:edad});
+});
+
+app.get('/productos',(req,res)=>{
+    const id = req.query.id;
+    const nombre = req.query.nombre;
+    res.status(200).json({id: id,nombre:nombre});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
