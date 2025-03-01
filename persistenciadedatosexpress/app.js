e4df53146965738f0ece2ee9d103fb991b7b06ff
jonie_mirc <jonie.mirc@gmail.com>
2025-02-28 19:28:57 -0600

    CRUD Completo, conexión a base de datos con express

--- persistenciadedatosexpress/app.js
+++ persistenciadedatosexpress/app.js
@@ -1,10 +1,13 @@
 const express=require('express');
+// instalar la libreria de mysql2 donde se encuentra el driver de conexión (npm install mysql2)
+// importar la libreria en express
 const mysql = require('mysql2');
 const app =express();
 const PORT =3000;
 
 app.use(express.json());
 
+// configurar las propiedades de la conexión a la base de datos
 const conexion = mysql.createPool({
     host:'localhost',
     user:'root',
@@ -29,6 +32,86 @@ app.get('/api/vehiculos/',(req,res)=>{
         }
 
         res.status(200).json({status:200,message:'Success',results});
+    });
+});
+
+app.post('/api/vehiculos',(req, res)=>{
+    let vehiculo = req.body;
+
+    if(!vehiculo.marca || !vehiculo.modelo || !vehiculo.anio){
+        return res.status(403).json({status:403,message:'Todos los campos son requeridos...'});
+    }
+    
+    const sql ="INSERT INTO vehiculo (marca,modelo,anio) VALUES(?,?,?)";
+    conexion.query(sql,[vehiculo.marca,vehiculo.modelo,vehiculo.anio],(err, results)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:500,message:'Error al insertar el registro...'});
+        }
+        vehiculo.codigo = results.insertId;
+        res.status(201).json({status:201,message:'Success',vehiculo});        
+    });
+
+});
+
+app.put('/api/vehiculos',(req,res)=>{
+    
+    let vehiculo = req.body;
+
+    if(!vehiculo.marca || !vehiculo.modelo || !vehiculo.anio || !vehiculo.codigo){
+        return res.status(403).json({status:403,message:'Todos los campos son requeridos...'});
+    }
+    
+    const sql ="UPDATE vehiculo SET marca =? , modelo=? , anio =? WHERE codigo =?";
+    conexion.query(sql,[vehiculo.marca,vehiculo.modelo,vehiculo.anio,vehiculo.codigo],(err, results)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:500,message:'Error al actualizar el registro...'});
+        }
+        
+        if(results.affectedRows === 0){
+            return res.status(404).json({status:404,message:'Vehiculo no encontrado...'});
+        }
+        
+        res.status(201).json({status:201,message:'Success',vehiculo});        
+    });
+});
+
+app.get('/api/vehiculos/:codigo',(req,res)=>{
+    let codigo = req.params.codigo;
+    if(!codigo){
+        return res.status(403).json({status:403,message:'El codigo del vehiculo es un parametro requerido...'});
+    }
+
+    const sql ="SELECT * FROM vehiculo WHERE codigo = ?";
+    conexion.query(sql,[codigo],(err, results)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:500,message:'Error al insertar el registro...'});
+        }
+        
+        res.status(200).json({status:200,message:'Success',results});        
+    });
+});
+
+app.delete('/api/vehiculos/:codigo',(req,res)=>{
+    let codigo = req.params.codigo;
+    if(!codigo){
+        return res.status(403).json({status:403,message:'El codigo del vehiculo es un parametro requerido...'});
+    }
+
+    const sql ="DELETE FROM vehiculo WHERE codigo = ?";
+    conexion.query(sql,[codigo],(err, results)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:500,message:'Error al insertar el registro...'});
+        }
+
+        if(results.affectedRows === 0){
+            return res.status(404).json({status:404,message:'Vehiculo no encontrado...'});
+        }
+        
+        res.status(201).json({status:201,message:'Registro eliminado con exito'});        
     });
 });
 
