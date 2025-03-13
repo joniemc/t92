53ad74bb1296ac7a8d0de7b694dee97f2cf53b17
jonie_mirc <jonie.mirc@gmail.com>
2025-03-12 18:57:29 -0600

    Update código clase 12 de marzo

--- persistenciadedatosexpress/middleware/authMiddleware.js
+++ persistenciadedatosexpress/middleware/authMiddleware.js
@@ -15,9 +15,7 @@ const authMiddleware = (req, res, next)=>{
         }
 
         next();
-    });
-
-    
+    });    
 };
 
 module.exports = authMiddleware;
--- persistenciadedatosexpress/routes/authUser.js
+++ persistenciadedatosexpress/routes/authUser.js
@@ -2,16 +2,18 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const pool = require('../config/db');
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const bcrypt = require('bcrypt');
 
 require('dotenv').config();
 
 
-router.post('/login',(req,res)=>{
+router.post('/login',async (req,res)=>{
     const {username,password} = req.body;
 
-    const sql = 'select * from tbl_usuarios where username = ? and password = ?';
+    const sql = 'select * from tbl_usuarios where username = ?';
 
-    pool.query(sql,[username,password],(err,resultado)=>{
+    pool.query(sql,[username], async (err,resultado)=>{
         if(err){
             return res.status(500).json({status:500,message:'Error del servidor'});
         }
@@ -21,12 +23,36 @@ router.post('/login',(req,res)=>{
         }
 
         let user = resultado[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(401).json({status:401,message:'Credenciales invalidas...'});
+        }
+
         const token = jwt.sign(
             {id: user.codigo, username: user.username},
             process.env.SECRET_KEY,
             {expiresIn: '1h'}
         );
         res.json({status:200,message:'Success',token});
+    });
+});
+
+router.post('/usuarios',authMiddleware ,async (req,res)=>{
+    const {username,password} = req.body;
+
+    const sql = 'insert into tbl_usuarios (username,password) values(?,?)';
+
+    const saltRound = 10;
+    const passwordEncrypt = await bcrypt.hash(password, saltRound);
+
+    pool.query(sql,[username,passwordEncrypt],  (err,resultado)=>{
+        if(err){
+            return res.status(500).json({status:500,message:'Error del servidor'});
+        }
+
+        res.json({status:200,message:'Success', codigo: resultado.insertId});
     });
 });
 
